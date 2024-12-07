2cfd61efcf0d10e153aca0a376059d561ad93ddb
JefferyAkorful <149057144+JefferyAkorful@users.noreply.github.com>
2024-12-07 09:05:21 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Set the PORT environment variable or default to 5000
 PORT=${PORT:-5000}
-
-
-
-# Launch Gunicorn with the application factory
 exec gunicorn "back:create_app()" --bind "0.0.0.0:${PORT}" -w 4
